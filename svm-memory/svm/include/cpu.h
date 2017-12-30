6f376db34fd572a6958572b441de5418da28970f
Ciri <haha@mail.ru>
2017-12-31 05:13:05 +0600

    cpu.h modified

--- svm-memory/svm/include/cpu.h
+++ svm-memory/svm/include/cpu.h
@@ -38,14 +38,14 @@ namespace svm
              *
              *   Add CPU opcode constants for memory load and store
              *   instructions.
-             *
-             *     static const int LDA_OPCODE = 0x40,
-             *                      LDB_OPCODE = 0x41,
-             *                      ...
-             *                      STA_OPCODE = 0x50,
-             *                      STB_OPCODE = 0x51
-             *                      ...
              */
+             static const int LDA_OPCODE = 0x40,
+                              LDB_OPCODE = 0x41,
+							  LDC_OPCODE = 0x42,
+							  STA_OPCODE = 0x50,
+							  STB_OPCODE = 0x51
+							  STC_OPCODE = 0x52;
+             
 
             Registers registers; // Current state of the CPU
 
